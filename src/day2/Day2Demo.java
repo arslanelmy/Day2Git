6ef5e359451df7fdcf77a288c3c694aa23671ca1
Phoenix <Phoenix@10.0.0.221>
2023-04-07 20:28:15 -0400

    initiated getInfo()

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -2,7 +2,7 @@ package day2;
 
 public class Day2Demo {
 
-	public static void main(String[] args) {
-		System.out.println("Demo 1, Day 2....");
+	public void getInfo() {
+		System.out.println("Pushing code...");
 	}
 }
